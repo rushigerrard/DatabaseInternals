998990a681eaf3feedbb4df7d3e5d945fc8467a5
rushigerrard <rsghatpa@ncsu.edu>
2016-12-13 00:20:48 -0500

    Cache replacement policies

---
+++ src/com/dbms/buffer/management/CLOCK.java
@@ -0,0 +1,108 @@
+package com.dbms.buffer.management;
+
+import java.util.HashMap;
+
+
+public class CLOCK {
+
+	private class Node{
+		int key; //will correspond to the actual data coming from the node
+		int value; // will be the index of element coming from the stream
+		boolean flag;
+		Node next;
+		
+		public Node(int key, int value){
+			this.key = key;
+			this.value = value;
+			this.flag = true;
+			this.next = null;
+		}
+		public Node(){
+			this.flag = true;
+			this.next = null;
+		}
+	}
+	
+	HashMap<Integer, Node> hm = new HashMap<>();
+	int cacheSize = 3;
+	Node pointer = null;
+	Node head;
+	
+	public CLOCK(int value){
+		if(value > 0)
+			this.cacheSize = value;
+		else
+			this.cacheSize = 3; //default
+		
+		head = new Node();
+		head.key = 0;
+		head.next = head;
+		pointer = head;
+		
+		for(int i = 1; i < this.cacheSize; i++){
+			Node current = new Node();
+			pointer.next = current;
+			current.key = i;
+			current.next = head;
+			pointer = current;
+			
+		}
+		pointer = pointer.next;
+	}
+
+	private void insert(int key, int value) {
+		//System.out.println(pointer.key + "," + pointer.flag);
+		Node current = new Node(key, value);
+		
+		
+		
+		if(hm.containsKey(key)){ //cache hit case
+			
+			System.out.println("Cache hit for " + key + "," + value);
+			Node old = hm.get(key);
+			old.flag = true;
+			
+		}else{// cache miss
+			
+			System.out.println("Disk I/O for " + key + "," + value);
+			if(hm.size() >= cacheSize){ //cache full, bring page in cache
+				
+				while(pointer.flag == true){
+					pointer.flag = false;
+					pointer = pointer.next;
+				}
+				hm.remove(pointer.key);
+				
+			}
+			hm.put(key, pointer);
+			pointer.flag = true;
+			pointer.key = key;
+			pointer.value = value;
+			pointer = pointer.next;
+		}
+	}
+	
+	
+	private void setHead(Node current) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void remove(Node node) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static void main(String[] args) {
+		//int[] stream = new int[]{4, 7, 0, 7, 1, 0, 1, 2, 1, 2, 7, 1};
+		//int[] stream = new int[]{1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4};
+		int[] stream = new int[]{2, 3, 2, 1, 5, 2, 4, 5, 3, 2, 5, 2};
+		CLOCK clock = new CLOCK(3);
+				
+		for(int i = 0; i < stream.length; i++){
+			clock.insert(stream[i], i);
+		}
+	}
+
+
+}
---
+++ src/com/dbms/buffer/management/MRU.java
@@ -0,0 +1,95 @@
+package com.dbms.buffer.management;
+
+import java.util.HashMap;
+
+public class MRU{
+
+	
+	private class Node{
+		int key; //will correspond to the actual data coming from the node
+		int value; // will be the index of element coming from the stream
+		Node prev;
+		Node next;
+		
+		public Node(int key, int value){
+			this.key = key;
+			this.value = value;
+			prev = null;
+			next = null;
+		}
+	}
+
+	HashMap<Integer, Node> hm = new HashMap<>();
+	int cacheSize = 3;
+	Node head = null;
+	Node tail = null;
+	
+	public MRU(int value){
+		
+		if(value > 0)
+			this.cacheSize = value;
+		else
+			this.cacheSize = 3; //default
+		
+	}
+	
+	public void insert(int key, int value){
+		Node current = new Node(key, value);
+		
+		if(hm.containsKey(key)){ //cache hit case
+			System.out.println("Cache hit for " + key + "," + value);
+			remove(hm.get(key));
+			hm.remove(key);
+			
+		}else{// cache miss
+			System.out.println("Disk I/O for " + key + "," + value);
+			if(hm.size() >= cacheSize){ //cache full, bring page in cache
+				hm.remove(head.key);
+				remove(head);
+			}
+			
+		}
+		setHead(current);
+		hm.put(key, current);
+	}
+	
+	
+	private void setHead(Node current) {
+		current.next = head;
+		current.prev = null;
+		if(head != null){
+			head.prev = current;
+		}
+		head = current;
+		if(tail == null)
+			tail = current;
+	}
+
+	private void remove(Node toRemove) {
+		if(toRemove.prev == null){	//Removing head 
+            head = toRemove.next;
+        }else{						//Removing otherwise
+        	toRemove.prev.next = toRemove.next;
+        }
+ 
+        if(toRemove.next == null){	//Removing tail
+        	tail = toRemove.prev;
+        }else{						//Removing otherwise
+        	toRemove.next.prev = toRemove.prev;
+        }
+ 
+	}
+
+	public static void main(String[] args) {
+		//int[] stream = new int[]{4, 7, 0, 7, 1, 0, 1, 2, 1, 2, 7, 1};
+		//int[] stream = new int[]{1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4};
+		int[] stream = new int[]{2, 3, 2, 1, 5, 2, 4, 5, 3, 2, 5, 2};
+		MRU mru = new MRU(3);
+		
+		for(int i = 0; i < stream.length; i++){
+			mru.insert(stream[i], i);
+		}
+		
+	}
+
+}
